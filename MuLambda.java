ba66b14a7470104ed1709576eb301c3f79ba825f
Albert <albert.cheu@gmail.com>
2015-10-26 21:09:58 -0400

    MuLambda got to level 3

---
+++ MuLambda.java
@@ -0,0 +1,162 @@
+package pacman.entries.pacman;
+
+import pacman.controllers.Controller;
+import pacman.controllers.examples.Legacy;
+import pacman.game.Constants.MOVE;
+import pacman.game.Game;
+import pacman.game.Constants.GHOST;
+import pacman.game.Constants.DM;
+
+import java.util.Random;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.ArrayList;
+
+public class MuLambda extends Controller<MOVE>{
+    private MOVE[] allMoves = MOVE.values();
+    private Legacy lg = new Legacy();
+    private int C = 4;
+    private int MU = 16;
+    private int LAMBDA = 20;
+
+    private Random rnd=new Random();
+
+    //Evaluates game state
+    private int score(Game state){
+	if (state.wasPacManEaten()) { return Integer.MIN_VALUE; }
+	int pnode = state.getPacmanCurrentNodeIndex();
+	int shortest2inedible = Integer.MAX_VALUE;
+	int shortest2edible = Integer.MAX_VALUE;
+
+	//Find distances to ghosts
+	for(GHOST g: GHOST.values()){
+	    int gnode = state.getGhostCurrentNodeIndex(g);
+	    int dist = state.getShortestPathDistance(pnode,gnode);
+	    if (state.isGhostEdible(g) && dist < shortest2edible)
+		{ shortest2edible = dist; }
+	    else if (!state.isGhostEdible(g) && dist < shortest2inedible)
+		{ shortest2inedible = dist; }
+	}
+
+	int shortest2pill = Integer.MAX_VALUE;
+	//Find distances to pills
+	for(int i: state.getActivePillsIndices()){
+	    int dist = state.getShortestPathDistance(pnode,i);
+	    if (dist < shortest2pill) { shortest2pill = dist; }
+	}
+
+	//Complete evaluation based on ghosts & score
+	if (shortest2edible == Integer.MAX_VALUE) { shortest2edible = 0; }
+	else if (shortest2inedible == Integer.MAX_VALUE) { shortest2inedible = 0; }
+	return state.getScore() + shortest2inedible - shortest2edible - shortest2pill;
+    }
+
+    private MOVE progress(ArrayList<Integer> choices, Game copy){
+	int c = 0;
+	MOVE firstMove = MOVE.NEUTRAL;
+	boolean first = true;
+	while(true){
+	    //...Until we die
+	    if (copy.wasPacManEaten()){ break; }
+
+	    //Find where we are
+	    int pnode = copy.getPacmanCurrentNodeIndex();
+
+	    MOVE lastMove = copy.getPacmanLastMoveMade();
+	    MOVE[] possible = copy.getPossibleMoves(pnode, lastMove);
+	    MOVE m = possible[0];
+
+	    //Make a turn
+	    if (possible.length > 1) {
+		if (c == choices.size()) { break; }
+		m = possible[choices.get(c++) % possible.length];
+	    }
+
+	    copy.advanceGame(m, lg.getMove(copy, System.currentTimeMillis()+1));
+	    if (first) { firstMove = m; first = false; }
+	}
+	return firstMove;
+    }
+
+    public MOVE getMove(Game game, long timeDue) {
+
+	/*
+	  Generate MU+LAMBDA random sequences of C integers between 0 and 5, inclusive
+	  For each, progress copy of state:
+	    Until the last number is reached, call current number c
+	    If dead, stop
+	    If there is only one possible move, obey it
+	    If there are two, pick the one denoted by c%2
+	    If three, pick the one denoted by c%3
+	  Evaluate
+
+	  Until out of time,
+  	    Sort the MU+LAMBDA sequences
+	    Replace the LAMBDA worst with random changes to the MU best sequences
+	    For each sequence, progress copy of game state and evaluate
+	  Return best first move
+	*/
+
+	class Individual{
+	    public MOVE firstMove;
+	    public int score;
+	    public ArrayList<Integer> choices;
+	    Individual(MOVE m, int i, ArrayList<Integer> al){
+		firstMove = m;
+		score = i;
+		choices = al;
+	    }
+	};
+
+	class CustomComparator implements Comparator<Individual>{
+	    public int compare(Individual a, Individual b){
+		if (a.score == b.score) { return 0; }
+		
+		//descending order: return how much bigger b is than a
+		if (a.score != Integer.MIN_VALUE && b.score != Integer.MIN_VALUE)
+		    { return b.score-a.score; }
+
+		//If a is MIN_VALUE,
+		if (a.score == Integer.MIN_VALUE) { return Integer.MAX_VALUE; }
+
+		//If b is MIN_VALUE,
+		return Integer.MIN_VALUE;
+	    }
+	}
+
+	ArrayList<Individual> population = new ArrayList<Individual>();
+
+	for (int i = 0; i < MU+LAMBDA; i++){
+	    Game copy = game.copy();
+
+	    //Make a random sequence of turns
+	    ArrayList<Integer> choices = new ArrayList<Integer>();
+	    for(int j = 0; j < C; j++){
+		choices.add(rnd.nextInt(6));		
+	    }
+
+	    //Take them
+	    MOVE firstMove = progress(choices, copy);
+	    int s = score(copy);
+	    population.add(new Individual(firstMove,s,choices));
+	}
+
+	while (timeDue-4 > System.currentTimeMillis()){
+	    Collections.sort(population,new CustomComparator());
+
+	    for(int i = 0; i < LAMBDA; i++){
+		ArrayList<Integer> choices = population.get(i%MU).choices;
+		int index = rnd.nextInt(C);
+		int val = rnd.nextInt(6);
+		choices.set(index,val);
+		Game copy = game.copy();
+		MOVE firstMove = progress(choices, copy);
+		int s = score(copy);
+		population.set(MU+i,new Individual(firstMove,s,choices));
+	    }
+	}
+
+	return population.get(0).firstMove;
+
+    }
+}
